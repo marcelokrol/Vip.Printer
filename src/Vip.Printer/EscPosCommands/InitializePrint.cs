8878b374ac78f29657dbd1eeba330ebb74fb259f
Marcelo Krol <marcelokrol@yahoo.com.br>
2020-08-08 17:12:52 -0300

    Correção do retorno

--- src/Vip.Printer/EscPosCommands/InitializePrint.cs
+++ src/Vip.Printer/EscPosCommands/InitializePrint.cs
@@ -13,8 +13,7 @@ namespace Vip.Printer.EscPosCommands
 
         private static byte[] SetLineSpace3(byte range = 30)
         {
-           var b = new byte[] { 27, '3'.ToByte(), range };
-           return b;
-        }
+           return new byte[] { 27, '3'.ToByte(), range };
+      }
    }
 }
